28bc958e424c8cccf60bedbc5883358915bcc606
suntoon <yuelifeng0415@aliyun.com>
2019-03-14 15:18:41 +0800

    '2019-03-14'

--- src/main/java/com/tileserver/util/GoogleTiles.java
+++ src/main/java/com/tileserver/util/GoogleTiles.java
@@ -1,6 +1,7 @@
 package com.tileserver.util;
 
-import com.sun.org.apache.bcel.internal.classfile.Code;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ProjectionName tileServer
@@ -85,13 +86,77 @@ public class GoogleTiles {
         return tileCode;
     }
 
+    /**
+     * @Author YueLifeng
+     * @Description //通过瓦片编号和级别获取瓦片的最小外包矩形（即瓦片范围）
+     * @Date 下午 2:25 2019/3/14 0014
+     * @param tileCode    行列号
+     * @param zoom   级别
+     * @return com.tileserver.util.Coordinate[]    最小外包矩形坐标
+     */
+    public static Coordinate[] tilecode2Coordinate(TileCode tileCode, final int zoom) {
+        int x = tileCode.getRow();
+        int y = tileCode.getColumn();
+        Coordinate c1 = new Coordinate();
+        c1.setLon(tile2lon(x, zoom));
+        c1.setLat(tile2lat(y, zoom));
+        Coordinate c2 = new Coordinate();
+        c2.setLon(tile2lon(x + 1, zoom));
+        c2.setLat(tile2lat(y + 1, zoom));
+        return new Coordinate[]{c1, c2};
+    }
 
+    /**
+     * @Author YueLifeng
+     * @Description //通过瓦片编号和级别获取纬度
+     * @Date 下午 2:23 2019/3/14 0014
+     * @param y   纬度
+     * @param zoom    级别
+     * @return double   纬度
+     */
+    private static double tile2lat(int y, int zoom) {
+        double n = Math.PI - (2.0 * Math.PI * y) / Math.pow(2.0, zoom);
+        return Math.toDegrees(Math.atan(Math.sinh(n)));
+    }
 
+    /**
+     * @Author YueLifeng
+     * @Description //通过瓦片编号和级别获取经度
+     * @Date 下午 2:21 2019/3/14 0014
+     * @param x    编号x
+     * @param zoom    级别
+     * @return double    经度
+     */
+    private static double tile2lon(int x, int zoom) {
+        return x / Math.pow(2.0, zoom) * 360.0 - 180;
+    }
 
-
-
-
-
-
-
+    /**
+     * @Author YueLifeng
+     * @Description //通过范围获取对应级别的所有瓦片编号
+     * @Date 下午 2:28 2019/3/14 0014
+     * @param coordinates   坐标
+     * @param level    级别
+     * @return java.util.List<com.tileserver.util.TileCode>   编号集合
+     */
+    public static List<TileCode> calculateCodes(Coordinate[] coordinates, int level) {
+        List<TileCode> tileCodeList = new ArrayList<>();
+        double xTileSize;
+        double yTileSize;
+        xTileSize = maxMLonRange / Math.pow(2, level);
+        yTileSize = maxMLatRange / Math.pow(2, level);
+        double xOrigin = coordinates[0].getLon();
+        double yOrigin = coordinates[0].getLat();
+        int xCount = (int) Math.ceil(Math.abs(coordinates[0].getLon() - coordinates[1].getLon()) / xTileSize);
+        int yCount = (int) Math.ceil(Math.abs(coordinates[0].getLat() - coordinates[1].getLat()) / yTileSize);
+        for (int i = 1; i <= xCount; i++) {
+            for (int j = 1; j <= yCount; j++) {
+                Coordinate coordinate = new Coordinate();
+                coordinate.setLon(xOrigin + xTileSize * (i - 1));
+                coordinate.setLat(yOrigin + yTileSize * (j - 1));
+                tileCodeList.add(coordinate2Tilecode(coordinate, level));
+            }
+        }
+        return tileCodeList.size() > 0 ? tileCodeList : null;
+    }
 }
